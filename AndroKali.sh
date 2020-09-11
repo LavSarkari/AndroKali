f360e0412708c79a6f81dbb40f1aef276a11651c
LavSarkari <69592007+LavSarkari@users.noreply.github.com>
2020-09-11 11:00:47 +0530

    Create AndroKali.sh

---
+++ AndroKali.sh
@@ -0,0 +1,74 @@
+#!/system/bin/sh
+# AndroKali by LavSarkari
+
+
+clear
+echo -e "\e[4;31m LavLab InfoSec !!! \e[0m"
+echo -e "\e[1;34m Presents \e[0m"
+echo -e "\e[1;32m AndroKali \e[0m"
+echo -e "\e[1;33m Coded by LavSarkari \e[0m"
+echo "Press Enter To Continue"
+read a1
+
+
+cd $HOME
+cd
+echo " "
+apt-get update -y
+echo " [$] Installation completed [10%]"
+pkg install wget -y
+echo " [$] Installation completed [29%]"
+pkg install python -y
+echo " [$] Installation completed [41%]"
+apt install figlet toilet -y
+echo " [$] Installation completed [53%]"
+pkg install python2 -y
+echo " [$] Installation completed [68%]"
+pkg install openssh -y
+echo " [$] Installation completed [82%]"
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh ls cd AndroKali
+echo " [$] Installation completed [99%]"
+ls
+echo " "
+cd AndroKali
+
+echo " [$] Installation completed...."
+echo Please wait.....\
+clear
+echo This Script is Created By LavSarkari
+sleep 0.4
+echo Please wait...../
+clear
+
+
+clear
+echo -e "\e[1;31m"
+figlet AndroKali                                                                                                                                       echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f mono12 -F border LavSarkari
+echo " "
+
+sleep 0.4
+
+
+echo Please wait...                                                                                                                                    echo "[Read carefully the command to run Kali Linux OS In VNC viewer...] "
+
+ls
+cat KaliinVNC
+clear
+$HOME
+ls
+cd $HOME
+ls
+
+echo" "
+
+ls
+cat KaliinVNC
+clear
+$HOME
+ls
+cd $HOME
+ls
+                                                                                                                                                       echo" "
+echo "[installation completed]"                                                                                                                        echo " "
+sleep 1
